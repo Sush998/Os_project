f0f76b50439a4d44086c42f39aed6966f6912c54
Sushma Singh <sushmamiku998@gmail.com>
2018-04-22 21:54:04 +0530

    1

---
+++ Osproject.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int at[10],bt[10],rt[10],endTime,i,smallest;
+    int remain=0,n,time,sum_wait=0,sum_turnaround=0;
+    printf("Enter no of Processes : ");
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        printf("Enter arrival time for Process P%d : ",i+1);
+        scanf("%d",&at[i]);
+        printf("Enter burst time for Process P%d : ",i+1);
+        scanf("%d",&bt[i]);
+        rt[i]=bt[i];
+    }
+    printf("\n\nProcess\t|Turnaround Time| Waiting Time\n\n");
+    rt[9]=9999;
+    
